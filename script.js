6ec54dd4fd8211369d71c1970522522d8fcb3640
Izabelle bones <03062280093@senacrs.edu.br>
2024-10-24 07:58:43 -0300

    RNA 2d

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * Based on this rad Dribbble shot: https://dribbble.com/shots/6280755-DNA-in-Motion
+ * Then remixed with the Jurassic park color scheme :D
+*/
+ 
+// If they're not supported, add them with JS
+if (!CSS.supports('top: calc(sin(1) * 1px)')) {
+    // Push the delays in
+    const STRANDS = [...document.querySelectorAll('.strand')]
+    for (let s = 0; s < STRANDS.length; s++) {
+      // --delay: calc(sin((var(--index) / var(--total)) * 45deg) * var(--speed) * -1s);
+      const DELAY = Math.sin(((Math.PI / 180) * 45) * ((s + 1) / STRANDS.length));
+      STRANDS[s].style.setProperty('--delay', `calc((${DELAY} * var(--speed)) * -1s)`);
+    }
+  }
+ 
+  // Set random colors
+  const COLORS = ['hsl(44, 98%, 60%)', 'hsl(197, 50%, 44%)', 'hsl(300, 100%, 100%)', 'hsl(331, 76%, 50%)']
+ 
+  const NODES = document.querySelectorAll('.strand__node')
+  NODES.forEach(NODE => {
+    NODE.style.setProperty('--bg', COLORS[Math.floor(Math.random() * COLORS.length)])
+  })
