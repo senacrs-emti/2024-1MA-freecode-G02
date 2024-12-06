1a87e6b47f592cd6897b5f2334dfffaab20b1a06
maria eduarda summchen <04595523092@senacrs.edu.br>
2024-12-06 11:40:37 -0300

    atualização

--- quiz.js
+++ quiz.js
@@ -1,10 +1,9 @@
 document.getElementById('playButton').addEventListener('click', function() {
-    window.location.href = 'gamedna.html'; // Altere para a URL da página que você deseja redirecionar
-});
+    window.location.href = 'gamedna.html'; 
 document.getElementById('playButton').addEventListener('click', function() {
     this.classList.add('fade-out'); // Adiciona a classe de animação
 
     setTimeout(() => {
-        window.location.href = 'sua_pagina_de_destino.html'; // Altere para a URL da página que você deseja redirecionar
-    }, 500); // Espera a animação terminar antes de redirecionar
+        window.location.href = 'sua_pagina_de_destino.html'; 
+    }, 500); 
 });
